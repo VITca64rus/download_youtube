d9b23549c258b07baf594c00777d39af7c43068c
Podlevskiy Viktor <vitca64rus@gmail.com>
2021-04-27 19:19:14 +0300

    make youtube download

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,27 +1,21 @@
 from token_keys import key_telegram
 import telebot
 import pytube
+import json
+import os
 
 bot = telebot.TeleBot(key_telegram)
+video = None
 
-
-def get_formats(yt):
-    str_format = 'Выберите качество видео:\n'
-    num = 1
-    formats = yt.streams.order_by ('resolution').filter (mime_type="video/mp4")
-    for format_ in formats:
-        str_format = str(num)+': ' + str_format + str(format_) + '\n'
-        num += 1
-    return str_format
-
-def take(n, seq):
-    result = []
-    try:
-        for i in range(n):
-            result.append(next(seq))
-    except StopIteration:
-        pass
-    return result
+def create_keyboard(yt):
+    buttons = []
+    keyboard = telebot.types.InlineKeyboardMarkup (row_width=1)
+    i=0
+    for stream in yt.streams.filter (mime_type='video/mp4'):
+        buttons.append (telebot.types.InlineKeyboardButton(text=str(stream), callback_data=str(i)))
+        i += 1
+    keyboard.add (*buttons)
+    return keyboard
 
 
 @bot.message_handler(commands=['start'])
@@ -43,10 +37,21 @@ def get_text_messages(message):
     except pytube.exceptions.RegexMatchError:
         bot.send_message(message.from_user.id, "Не могу скачать данный фаил. Проверьте ссылку")
     if yt is not None:
-        pass
-        #print(yt.streams[0].desc())
-        #bot.send_message(message.from_user.id, get_formats(yt)+'Пришлите номер выбранного фаила')
+        keyboard = create_keyboard(yt)
+        global video
+        video = yt
+        bot.send_message(message.from_user.id,'Выберете формат для загрузки', reply_markup = keyboard)
 
+@bot.callback_query_handler(func=lambda x: True)
+def callback_handler(callback_query):
+    global video
+    bot.send_message (callback_query.from_user.id, "Подождите, началась загрузка...")
+    #video.streams[int(callback_query.data)].download()
+    #bot.send_message (callback_query.from_user.id, "Фаил загружен на сервер")
+    video = open(video.streams[int(callback_query.data)].download(filename='{}'.format(callback_query.from_user.id)), 'rb')
+    bot.send_video (callback_query.from_user.id, video)
+    video.close()
+    os.remove('{}.mp4'.format(callback_query.from_user.id))
 
 
 bot.polling(none_stop=True, interval=0)
