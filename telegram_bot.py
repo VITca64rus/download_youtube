035a08721814f0bdbf3b6a95a9d0501f9c6fe3df
vitca64rus <vitca64rus@gmail.com>
2022-03-19 21:56:05 +0300

    add info

--- telegram_bot.py
+++ telegram_bot.py
@@ -2,7 +2,7 @@ import telebot
 import pytube
 import os
 
-bot = telebot.TeleBot(1767984719:AAExB36Biu1bfL6h8G_MxzOZ789tiuUg648)
+bot = telebot.TeleBot('1767984719:AAExB36Biu1bfL6h8G_MxzOZ789tiuUg648')
 video = None
 
 
